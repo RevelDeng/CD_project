abb6d3d049e7dafef6b2a1e992f031e7197a2637
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-02-05 18:10:34 -0500

    ecommerce app, categories

---
+++ login_app/views.py
@@ -0,0 +1,178 @@
+from django.shortcuts import render, redirect
+from .models import User, Admin
+from django.contrib import messages
+import bcrypt
+from django.apps import apps
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def marketplace(request):
+    if 'user_id' in request.session:
+        shop_items = apps.get_model('marketplace.Item').objects.all()
+        if shop_items:
+            user_items = apps.get_model('marketplace.User_Item_Count').objects.filter(user=User.objects.get(id=request.session['user_id']))
+            if user_items:
+                total_carbon_offset = 0
+                total_cost = 0
+                for item in user_items:
+                    total_carbon_offset += item.item.carbon_offset_in_trees * item.orders
+                    total_cost += item.item.cost * item.orders
+                context = {
+                    'items': reversed(shop_items),
+                    'user': User.objects.get(id=request.session['user_id']),
+                    'user_items': user_items,
+                    'total_carbon_offset': total_carbon_offset,
+                    'total_cost': total_cost
+                }
+                return render(request, 'marketplace.html', context)
+            else:
+                context = {
+                    'items': reversed(shop_items),
+                    'user': User.objects.get(id=request.session['user_id']),
+                }
+                return render(request, 'marketplace.html', context)
+        else:
+            context = {
+                'user': User.objects.get(id=request.session['user_id'])
+            }
+            return render(request, 'marketplace.html', context)
+    else:
+        return redirect('index')
+
+def add_user(request):
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags='register')
+        return redirect('index')
+    else:
+        User.objects.create(
+            first_name=request.POST["first_name"], last_name=request.POST["last_name"], email=request.POST["email"], password=bcrypt.hashpw(
+                request.POST["password"].encode(), bcrypt.gensalt()
+            ).decode()
+        )
+        request.session['user_id'] = User.objects.last().id
+        return redirect('marketplace')
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags="login")
+        return redirect('index')
+    else:
+        user = User.objects.filter(email=request.POST['email_login'])
+        if user:
+            logged_user = user[0]
+            if bcrypt.checkpw(request.POST['pw_login'].encode(), logged_user.password.encode()):
+                request.session['user_id'] = logged_user.id
+                return redirect('marketplace')
+            else:
+                messages.error(request, "Incorrect password.", extra_tags="login")
+                return redirect('index')
+        else:
+            messages.error(request, "Incorrect email.", extra_tags="login")
+            return redirect('index')
+
+def logout(request):
+    del request.session['user_id']
+    return redirect('index')
+
+def admin_index(request):
+    return render(request, 'admin_index.html')
+
+def admin_page(request):
+    if 'admin_id' in request.session:
+        categories = apps.get_model('marketplace.Category').objects.all()
+        if categories:
+            shop_items = apps.get_model('marketplace.Item').objects.all()
+            if shop_items:
+                user_items = apps.get_model('marketplace.User_Item_Count').objects.all()
+                if user_items:
+                    context = {
+                        'admin': Admin.objects.get(id=request.session['admin_id']),
+                        'items': reversed(shop_items),
+                        'user_items': user_items,
+                        'users': User.objects.all(),
+                        'categories': categories
+                    }
+                    return render(request, 'admin_page.html', context)
+                else:
+                    context = {
+                        'admin': Admin.objects.get(id=request.session['admin_id']),
+                        'items': reversed(shop_items),
+                        'categories': categories
+                    }
+                    return render(request, 'admin_page.html', context)
+            else:
+                context = {
+                    'admin': Admin.objects.get(id=request.session['admin_id']),
+                    'categories': categories
+                }
+                return render(request, 'admin_page.html', context)
+        else:
+            shop_items = apps.get_model('marketplace.Item').objects.all()
+            if shop_items:
+                user_items = apps.get_model('marketplace.User_Item_Count').objects.all()
+                if user_items:
+                    context = {
+                        'admin': Admin.objects.get(id=request.session['admin_id']),
+                        'items': reversed(shop_items),
+                        'user_items': user_items,
+                        'users': User.objects.all()
+                    }
+                    return render(request, 'admin_page.html', context)
+                else:
+                    context = {
+                        'admin': Admin.objects.get(id=request.session['admin_id']),
+                        'items': reversed(shop_items)
+                    }
+                    return render(request, 'admin_page.html', context)
+            else:
+                context = {
+                    'admin': Admin.objects.get(id=request.session['admin_id'])
+                }
+                return render(request, 'admin_page.html', context)
+    else:
+        return redirect('admin_index')
+
+def add_admin(request):
+    errors = Admin.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags='register')
+        return redirect('admin_index')
+    else:
+        Admin.objects.create(
+            first_name=request.POST["first_name"], last_name=request.POST["last_name"], email=request.POST["email"], password=bcrypt.hashpw(
+                request.POST["password"].encode(), bcrypt.gensalt()
+            ).decode()
+        )
+        request.session['admin_id'] = Admin.objects.last().id
+        return redirect('admin_page')
+
+def admin_login(request):
+    errors = Admin.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags="login")
+        return redirect('admin_index')
+    else:
+        admin = Admin.objects.filter(email=request.POST['email_login'])
+        if admin:
+            logged_admin = admin[0]
+            if bcrypt.checkpw(request.POST['pw_login'].encode(), logged_admin.password.encode()):
+                request.session['admin_id'] = logged_admin.id
+                return redirect('admin_page')
+            else:
+                messages.error(request, "Incorrect password.", extra_tags="login")
+                return redirect('admin_index')
+        else:
+            messages.error(request, "Incorrect email.", extra_tags="login")
+            return redirect('admin_index')
+
+def admin_logout(request):
+    del request.session['admin_id']
+    return redirect('admin_index')
---
+++ marketplace/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+
+# Create your models here.
+class ItemManager(models.Manager):
+    def validate(self, postData):
+        errors = {}
+        if len(postData['item_name']) < 2:
+            errors['item_name'] = "New item name must be at least 2 characters."
+        for item in Item.objects.all():
+            if postData['item_name'] == item.name:
+                errors['unique_item_name'] = "An item with this name has already been created."
+        if len(postData['item_description']) < 2:
+            errors['item_description'] = "New item description must be at least 2 characters."
+        return errors
+    def validate_edit(self, postData):
+        errors = {}
+        if len(postData['item_name']) < 2:
+            errors['item_name'] = "New item name must be at least 2 characters."
+        if len(postData['item_description']) < 2:
+            errors['item_description'] = "New item description must be at least 2 characters."
+        return errors
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    carbon_offset_in_trees = models.IntegerField()
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    purchases = models.ManyToManyField('login_app.User', through="User_Item_Count")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = ItemManager()
+    def __repr__(self) -> str:
+        return f"<Item object: {self.name} {self.categories} ({self.id})>"
+
+class User_Item_Count(models.Model):
+    user = models.ForeignKey('login_app.User', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    orders = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __repr__(self) -> str:
+        return f"<User_Item_Count object: {self.user.first_name} {self.item.name} {self.orders} ({self.id})>"
+
+class CategoryManager(models.Manager):
+    def validate(self, postData):
+        errors = {}
+        if len(postData['category_name']) < 2:
+            errors['category_name'] = "New category name must be at least 2 characters."
+        for category in Category.objects.all():
+            if postData['category_name'] == category.name:
+                errors['unique_category_name'] = "A category with this name has already been created."
+        if len(postData['category_description']) < 2:
+            errors['category_description'] = "New category description must be at least 2 characters."
+        return errors
+    def validate_edit(self, postData):
+        errors = {}
+        if len(postData['category_name']) < 2:
+            errors['category_name'] = "New category name must be at least 2 characters."
+        if len(postData['category_description']) < 2:
+            errors['category_description'] = "New category description must be at least 2 characters."
+        return errors
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    items = models.ManyToManyField(Item, related_name="categories")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = CategoryManager()
+    def __repr__(self) -> str:
+        return f"<Category object: {self.name} {self.items} ({self.id})>"
