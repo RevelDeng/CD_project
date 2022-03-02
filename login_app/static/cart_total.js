53853e64bebec5eb91c75f9eda228f44a59c9f8c
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-03-02 09:00:12 -0500

    adding React component for cart total

---
+++ login_app/static/cart_total.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+function cart_total() {
+    const [total, setTotal] = useState("")
+
+    return Cart total: {total} items
+}
+
+ReactDOM.render(
+    <cart_total/>,
+    document.getElementById('cart_total')
+)
